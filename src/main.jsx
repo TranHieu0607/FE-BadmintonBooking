7f0d4a6ebef4e02e1c4438e294c7486d39be6a0b
nmytran <n.mytran12@gmail.com>
2024-11-05 14:01:13 +0700

    add missing code

--- src/main.jsx
+++ src/main.jsx
@@ -27,9 +27,9 @@ const router = createBrowserRouter([
       {
         path: "profile",
         element: <Profile />,
-      }
+      },
       {
-        path: "/admin",
+        path: "admin",
         element: <AdminDashboard />,
       },
     ],
